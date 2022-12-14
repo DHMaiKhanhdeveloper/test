7d9b47357ee7ba4f6a9f73de31dfb1b470c64c64
UIT.19521675 <70103187+MaiKhanh19521675@users.noreply.github.com>
2022-12-14 12:56:23 +0700

    first commit

---
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
